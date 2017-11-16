719c1f92be32cd239d7842b6485f65f1a4cb5b70
Ashe Austaire <AsheAustaire@gmail.com>
2017-11-16 00:29:55 -0500

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,11 @@
 export function manageFriends(state, action){
+  switch (action.type) {
+    case 'ADD_FRIEND' :
+    return {...state, friends: [...state.friends, action.friend]}
+    case 'REMOVE_FRIEND' :
+    let friendsCopy = [...state.friends]
+    return {...state, friends: friendsCopy.filter((fr) => {return fr.name != 'Joe'})}
+    default :
+      return {...state}
+  }
 }
